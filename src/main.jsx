a3770dd51ff44bc4b4dd359700142598edfdc4f7
ANJALI PILLAI <anju2604lp@gmail.com>
2024-09-29 11:24:43 +0530

    Add routing functionality

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router=createBrowserRouter([
+  {
+    path: '/',
+    element:<App/>
+  },
+  {
+    path: '/create-trip',
+    element:<CreateTrip/>
+  }
+])
+
+React.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
